20d4a89b6d38912be2fc1cbfbf1e07119991fe59
EBillecocq <ernest.billecocq@2025.icam.fr>
2024-06-04 15:15:08 +0200

    changement attribut spec_values

--- transformateur.py
+++ transformateur.py
@@ -3,14 +3,12 @@ from composant import Composant
 class Transformateur(Composant):
     def __init__(self, coords):
         super().__init__(coords)
-        self.spec_values = {
-            "coords" : {
-                "puissanceNominale" : (220, 812),
-                "tensionPrimaire" : (211, 840),
-                "tensionSecondaireAVide" : (212, 871),
-                "Reseau" : (423, 871),
-                "chuteDeTensionAdmissible" : (150, 953)
-        }
+        self.spec_values = { 
+            "puissanceNominale" : (220, 812),
+            "tensionPrimaire" : (211, 840),
+            "tensionSecondaireAVide" : (212, 871),
+            "Reseau" : (423, 871),
+            "chuteDeTensionAdmissible" : (150, 953)     
         }
 
 if __name__ == "__main__":  
